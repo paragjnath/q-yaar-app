788dcb0520af9754c522dbf4be4e46256b438a0a
Parag Jyoti Nath <paragjnath@gmail.com>
2024-08-27 02:45:05 +0530

    fixes web issues due to async main function

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:q_yaar/constants/environment_config.dart';
 import 'package:q_yaar/ui/q_yaar_app.dart';
 
-void main() async {
-  await EnvironmentConfig.ensureInitilized();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  EnvironmentConfig.ensureInitilized();
   runApp(const QYaarApp());
 }
